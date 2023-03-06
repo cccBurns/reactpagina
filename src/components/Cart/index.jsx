a12ddf028b683001f578c990c54bd30a6a15554d
christian burns <christianburns03@gmail.com>
2023-03-06 15:46:54 -0600

    listo el carrito

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { useCartContext } from "../../context/CartContext";
 import ItemCart from "../ItemCart";
 
